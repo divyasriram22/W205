c5fa0d1e1ee93cf31dd1cb1a6b6eea1771121e4d
divyasriram22 <divyasriram@berkeley.edu>
2017-02-23 01:43:16 -0800

    update load_data_lake

--- Exercise 1/Loading_and_Modeling/load_data_lake.sh
+++ Exercise 1/Loading_and_Modeling/load_data_lake.sh
@@ -1,61 +1,136 @@
-#Divya Sriram W205 Exercise 1
-#commands that I used in my terminal for the beginning of exercise 1
-
-#removing spaces in file names (changing to _ )
-
-for file in *; do mv "$file" `echo $file | tr ' ' '_' ; done
-
-#creating a directory for each file
-	#otherwise hive will pull all tables in one directory, so need separate one for each
-
-find . -n "*.csv" -exec sh - c 'NEWDIR=`basename "$1" .csv` ; mkdir "$NEWDIR" ; mv "$" "$NEWDIR" ' _ {} \;
-
-#put all files into git hub
-git clone https://github.com/divyasriram22/W205-GitHub.git
+#DIVYA SRIRAM W205 EXERCISE 1 LOAD_DATA_LAKE.SH
+# followed amit's example -- this is such a convenient way to do it!!
 
 
-for d in*; do
- if [ -d "$d" ]; then        
-   ( cd "$d" && for x in *.csv; do sed '1d' "$x" > tmpfile; mv tmpfile "$x"; done )
- fi
-done
-
-#change names of important files
-
-mv /home/w205/w205_exercise_1/Hospital_General_Information/Hospital_General_Information.csv /home/w205/w205_exercise_1/Hospital_General_Information/hospitals.csv
-
-mv /home/w205/w205_exercise_1/Timely_and_Effective_Care_-_Hospital/Timely_and_Effective_Care_-_Hospital.csv /home/w205/w205_exercise_1/Timely_and_Effective_Care_-_Hospital/effective_care.csv
-
-mv /home/w205/w205_exercise_1/Readmissions_and_Deaths_-_Hospital/Readmissions_and_Deaths_-_Hospital.csv /home/w205/w205_exercise_1/Readmissions_and_Deaths_-_Hospital/readmissions.csv
+# INITIAL INSTRUCTIONS (script assumes user has done this)
+# starting at /home/w205/hospital_compare
 
 
-mv /home/w205/w205_exercise_1/Measure_Dates/Measure_Dates.csv /home/w205/w205_exercise_1/Measure_Dates/Measure.csv
+#!/bin/bash
 
-mv /home/w205/w205_exercise_1/hvbp_hcahps_05_28_2015/hvbp_hcahps_05_28_2015.csv /home/w205/w205_exercise_1/hvbp_hcahps_05_28_2015/surveys_responses.csv
+# MAKE NEEDED DIRECTORIES BOTH IN AWS AND HDFS
+
+mkdir raw_data
+mkdir workable_data
+hdfs dfs -mkdir hospital_compare
+
+# DOWNLOADING DATA
+wget  https://data.medicare.gov/views/bg9k-emty/files/6c902f45-e28b-42f5-9f96-ae9d1e583472?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 
 
-#take a look at hdfs current state
+unzip 6c902f45-e28b-42f5-9f96-ae9d1e583472?content_type=application%2Fzip; charset=binary
 
-hdfs dfs -ls
+rm 6c902f45-e28b-42f5-9f96-ae9d1e583472?content_type=application%2Fzip; charset=binary
 
-#make directories inside hospital_compare in hadoop for each csv file
 
-hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
-hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
-hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
-hdfs dfs -mkdir /user/w205/hospital_compare/Measures
-hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+#RENAMING FILE NAMES WITHOUT SPACES
 
-# put important files into the directories I created in Hospital Compare in HDFS
-	#creating a hdfs data lake!!
+for file in *; do mv "$file" `echo $file | tr ' ' '_'` ; done
 
-hdfs dfs -put /home/w205/w205_exercise_1/Hospital_General_Information/hospitals.csv /user/w205/hospital_compare/hospitals
 
-hdfs dfs -put /home/w205/w205_exercise_1/Timely_and_Effective_Care_-_Hospital/effective_care.csv /user/w205/hospital_compare/effective_care
+##### EDIT THE 5 MOST IMPORTANT FILES ##
 
-hdfs dfs -put /home/w205/w205_exercise_1/Readmissions_and_Deaths_-_Hospital/readmissions.csv /user/w205/hospital_compare/readmissions
+cd /home/w205/hospital_care
+mkdir
 
-hdfs dfs -put /home/w205/w205_exercise_1/Measure_Dates/Measure.csv /user/w205/hospital_compare/Measures
+HOSP_DIREC = '/home/w205/hospital_care/'
+RAW_DATA = $HOSP_DIRECT'raw_data/'
+RENAME_H_STRIP_DATA = $HOSP_DIRECT'workable_data/' # data files here are renamed, header stripped, no spaces
 
+
+#create directory
+
+####### HOSPITALS.CSV ######
+
+raw_data_file = 'Hospital_General_Information.csv'
+rename_h_strip_file = 'hospitals.csv' 
+hdfs_direc = hospitals
+
+cp "$RAW_DATA$raw_data_file" "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+tail -n +2 "RENAME_H_STRIP_DATA$rename_h_strip_file" > "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+
+ls $RENAME_H_STRIP_DATA
+
+#create directory in hdfs and move file to hdfs
+
+hdfs dfs -mkdir hospital_compare/$hdfs_dir
+hdfs dfs -put "$RENAME_H_STRIP_DATA$rename_h_strip_file" hospital_compare/$hdfs_dir
+hdfs dfs -ls hospital_compare/$hdfs_dir
+
+
+
+##### EFFECTIVE_CARE.CSV #####
+
+raw_data_file = 'Timely_and_Effective_Care_-_Hospital.csv'
+rename_h_strip_file = 'effective_care.csv'
+hdfs_direc = effective_care
+
+cp "$RAW_DATA$raw_data_file" "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+tail -n +2 "RENAME_H_STRIP_DATA$rename_h_strip_file" > "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+
+ls $RENAME_H_STRIP_DATA
+
+#create directory in hdfs and move file to hdfs
+
+hdfs dfs -mkdir hospital_compare/$hdfs_dir
+hdfs dfs -put "$RENAME_H_STRIP_DATA$rename_h_strip_file" hospital_compare/$hdfs_dir
+hdfs dfs -ls hospital_compare/$hdfs_dir
+
+
+
+#### READMISSIONS.CSV #######
+
+
+raw_data_file = 'Readmissions_and_Deaths_-_Hospital.csv'
+rename_h_strip_file = 'readmissions.csv'
+hdfs_direc = readmissions
+
+cp "$RAW_DATA$raw_data_file" "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+tail -n +2 "RENAME_H_STRIP_DATA$rename_h_strip_file" > "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+
+ls $RENAME_H_STRIP_DATA
+
+#create directory in hdfs and move file to hdfs
+
+hdfs dfs -mkdir hospital_compare/$hdfs_dir
+hdfs dfs -put "$RENAME_H_STRIP_DATA$rename_h_strip_file" hospital_compare/$hdfs_dir
+hdfs dfs -ls hospital_compare/$hdfs_dir
+
+
+##### MEASURES.CSV #########
+
+
+raw_data_file = 'Measure_Dates.csv'
+rename_h_strip_file = 'measures.csv'
+hdfs_direc = measures
+
+cp "$RAW_DATA$raw_data_file" "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+tail -n +2 "RENAME_H_STRIP_DATA$rename_h_strip_file" > "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+
+ls $RENAME_H_STRIP_DATA 
+
+#create directory in hdfs and move file to hdfs
+
+hdfs dfs -mkdir hospital_compare/$hdfs_dir
+hdfs dfs -put "$RENAME_H_STRIP_DATA$rename_h_strip_file" hospital_compare/$hdfs_dir
+hdfs dfs -ls hospital_compare/$hdfs_dir
+
+
+##### SURVEYS_RESPONSES ########
+
+raw_data_file = 'hvbp_hcahps_11_10_2016.csv'
+rename_h_strip_file = 'survey_responses.csv'
+hdfs_direc = survey_responses
+
+cp "$RAW_DATA$raw_data_file" "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+tail -n +2 "RENAME_H_STRIP_DATA$rename_h_strip_file" > "$RENAME_H_STRIP_DATA$rename_h_strip_file"
+
+ls $RENAME_H_STRIP_DATA
+
+#create directory in hdfs and move file to hdfs
+
+hdfs dfs -mkdir hospital_compare/$hdfs_dir
+hdfs dfs -put "$RENAME_H_STRIP_DATA$rename_h_strip_file" hospital_compare/$hdfs_dir
+hdfs dfs -ls hospital_compare/$hdfs_dir
 
 
